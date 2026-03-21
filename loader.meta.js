8ad68ae33db3f5a06d1ecf11e3f52165f8b7eeef
pixelGRX <gorokhovf1@gmail.com>
2026-03-21 20:09:02 +0300

    upd

--- loader.meta.js
+++ loader.meta.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Rabotyaga
 // @namespace    http://tampermonkey.net/
-// @version      0.0.4
+// @version      0.0.5
 // @description  goroxels.ru (пиксели покруче)
 // @author       Gorox
 // @match        https://pixel.xn--d1ah4a.com/
@@ -11,7 +11,7 @@
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
 // @connect      raw.githack.com
-// @connect      *.trycloudflare.com
+// @connect      trycloudflare.com
 // @updateURL    https://raw.githack.com/pixelGRX/itdStuff/main/loader.meta.js
 // @downloadURL  https://raw.githack.com/pixelGRX/itdStuff/main/loader.user.js
 // @run-at       document-start
